090d8a77aefe98375fba3a2fed5753bbaefe19a7
Luc Nguyen <luc.nguyen@levincigroup.com>
2021-09-22 22:20:58 +0700

    first commit

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const queryString = require('query-string');
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
+  res.jsonp(req.query);
+});
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === 'PATCH') {
+    req.body.updatedAt = Date.now();
   }
   // Continue to JSON Server router
-  next()
-})
+  next();
+});
+
+router.render = (req, res) => {
+  const header = res.getHeaders();
+  const totalCount = header['x-total-count'];
+
+  if (req.method === 'GET' && totalCount) {
+   const queryParams = queryString.parse(req._parsedOriginalUrl.query);
+    return res.jsonp({
+      data: res.locals.data,
+      pagination: {
+        _page: +queryParams._page || 1,
+        _limit: +queryParams._limit || 10,
+        _totalRows: +totalCount,
+      },
+    });
+  }
+
+  res.jsonp({ data: res.locals.data });
+};
 
 // Use default router
-server.use('/api',router)
+server.use('/api', router);
 server.listen(3000, () => {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
